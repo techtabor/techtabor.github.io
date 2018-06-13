e7bfc476911997b475f94a8da8d7029c735e12c2
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2018-06-13 09:46:51 +0200

    Started a basic framework of creating simple interactive javascript dojos.

---
+++ js/dojo.js
@@ -0,0 +1,106 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
+editor.setOptions({
+    //enableBasicAutocompletion: true,
+    //enableSnippets: true,
+    //enableLiveAutocompletion: true,
+    fontSize: 20 
+});
+
+var line_number = 0;
+var lines = [];
+function input() {
+    if (line_number < lines.length) {
+        return lines[line_number++];
+    } else {
+        return "";
+    }
+}
+
+function print(s) {
+    let new_value = $("#stdout-text").val() + s;
+    $("#stdout-text").val(new_value);
+}
+
+function runWithDelay() {
+    $("#notification-area").hide();
+    $("#stdout-text").val("");
+    setTimeout(run, 400);
+}
+
+function run() {
+    console.log("Running program...");
+    $("#stdout-text").val("");
+    lines = $("#stdin-text").val().split('\n');
+    console.log("Input lines: " + lines);
+    line_number = 0;
+    let code = editor.getValue();
+    try {
+        $("#notification-area").hide();
+        eval(code);
+        $("#notification-area").text("A programod lefutott!");
+        $("#notification-area").addClass("text-success");
+        $("#notification-area").removeClass("text-danger");
+        $("#notification-area").fadeIn(400);
+
+    } catch (err) {
+        $("#notification-area").text("Hiba a program futása során:  " + err.message);
+        $("#notification-area").addClass("text-danger");
+        $("#notification-area").fadeIn(400);
+    }
+}
+
+function startTest() {
+    $(".test-input-text").removeClass("test-failed");
+    $(".test-input-text").removeClass("test-passed");
+    $(".test-output-text").removeClass("test-failed");
+    $(".test-output-text").removeClass("test-passed");
+    $(".test-panel").removeClass("panel-success");
+    $(".test-panel").removeClass("panel-danger");
+    $("#stdout-text").val("");
+    setTimeout(function() { chainedTest(1); }, 400);
+}
+
+function singleTest(k) {
+    console.log("Running test #" + k);        
+    let in_id = "test-input-" + k;
+    let out_id = "test-output-" + k;
+    let test_input = $("#" + in_id).text();
+    console.log("Testing with input: " + test_input);
+    $("#stdin-text").val(test_input);
+    run();
+    let expected_output = $("#" + out_id).text();
+    if ($("#stdout-text").val() != expected_output) {
+        console.log("Test #" + k + " failed");
+        $("#" + in_id).addClass("test-failed");
+        $("#" + out_id).addClass("test-failed");
+        $("#test-panel-" + k).addClass("panel-danger");
+        $("#" + in_id).removeClass("test-passed");
+        $("#" + out_id).removeClass("test-passed");
+        $("#test-panel-" + k).removeClass("panel-success");
+        $("#notification-area").append("<br>Helytelen a kimenet a(z) " + k + ". tesztre.");
+        $("#notification-area").addClass("text-danger");
+        return false;
+    }
+    $("#" + in_id).addClass("test-passed");
+    $("#" + out_id).addClass("test-passed");
+    $("#test-panel-" + k).addClass("panel-success");
+    $("#" + in_id).removeClass("test-failed");
+    $("#" + out_id).removeClass("test-failed");
+    $("#test-panel-" + k).removeClass("panel-danger");
+    return true;
+}
+
+function chainedTest(k) {
+    let test_cnt = $(".test-input-text").length;
+    if (k > test_cnt) {
+        console.log("All tests succeded :)");
+    } else {
+        if (singleTest(k)) {
+            if (k < test_cnt) setTimeout(function() { $("#stdout-text").val(""); }, 400)
+            // Call next test with some delay.
+            setTimeout(function() { chainedTest(k + 1); }, 800);
+        }
+    }
+}
